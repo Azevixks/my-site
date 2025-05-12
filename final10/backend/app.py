566aaeab92737eba78a8ed4d97a41d513107c840
Azeviks <p.oleksandr@ortlyc.kyiv.ua>
2025-05-12 09:03:02 +0000

    123

--- final10/backend/app.py
+++ final10/backend/app.py
@@ -63,4 +63,5 @@ def get_supermarket_prices_filtered(class_name=None):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))  # Use the PORT environment variable or default to 5000
+    app.run(host='0.0.0.0', port=port)
